2402fa578bad388b1e26c05e07d8b00c0816ba5c
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-01-20 19:30:53 +0100

    Add a way of creating a useful User-Agent header

---
+++ E621Client/E621UserAgent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Noppes.E621
+{
+    /// <summary>
+    /// User-Agent sent with each request to e621/e926.
+    /// </summary>
+    public class E621UserAgent
+    {
+        private List<E621UserAgentPart> UserAgentParts { get; }
+
+        public E621UserAgent(string productName, string productVersion, string username,
+            string platform, string? location = null)
+        {
+            UserAgentParts = new List<E621UserAgentPart>
+            {
+                new E621UserAgentPart(productName, productVersion, username, platform, location),
+                Project.AsUserAgentPart()
+            };
+        }
+
+        public override string ToString()
+        {
+            StringBuilder userAgentBuilder = new StringBuilder();
+
+            for (int i = 0; i < UserAgentParts.Count; i++)
+            {
+                UserAgentParts[i].AppendString(userAgentBuilder);
+
+                if (i != UserAgentParts.Count - 1)
+                    userAgentBuilder.Append(" using ");
+            }
+
+            return userAgentBuilder.ToString();
+        }
+    }
+}
---
+++ E621Client/Exceptions/ArgumentNullOrWhiteSpaceException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Noppes.E621
+{
+    /// <summary>
+    /// Thrown whenever a string parameter is null or consists only out of whitespace characters.
+    /// </summary>
+    public class ArgumentNullOrWhiteSpaceException : ArgumentException
+    {
+        public ArgumentNullOrWhiteSpaceException(string paramName)
+            : base($"The parameter \"{paramName}\" may not be null or consist only out of whitespace characters.", paramName)
+        {
+        }
+    }
+}
