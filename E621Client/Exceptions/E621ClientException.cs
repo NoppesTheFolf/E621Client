d269172689a598ee57918315c82fe6a807642b8d
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-01-21 22:26:06 +0100

    Add some exception handling

---
+++ E621Client/Exceptions/E621ClientException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Noppes.E621
+{
+    public class E621ClientException : Exception
+    {
+        public E621ClientException()
+        {
+        }
+
+        public E621ClientException(string message) : base(message)
+        {
+        }
+
+        public E621ClientException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        internal static E621ClientException Create(Exception innerException) =>
+            new E621ClientException("An unknown error has occured.", innerException);
+    }
+}
---
+++ E621Client/Exceptions/E621ClientExceptionHelper.cs
@@ -0,0 +1,26 @@
+﻿using Flurl.Http;
+using System;
+using System.Net;
+
+namespace Noppes.E621
+{
+    public static class E621ClientExceptionHelper
+    {
+        public static E621ClientException FromException(Exception exception)
+        {
+            if (exception is E621ClientException clientException)
+                return clientException;
+
+            if (!(exception is FlurlHttpException httpException))
+                return E621ClientException.Create(exception);
+
+            if (exception is FlurlHttpTimeoutException)
+                return E621ClientTimeoutException.Create(exception);
+
+            if (httpException.Call.Response.StatusCode == HttpStatusCode.Forbidden)
+                return E621ClientUnauthorizedException.Create(exception);
+
+            return E621ClientException.Create(exception);
+        }
+    }
+}
