16c756c3ce3af79efbab83a2c4319782df394660
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-01-21 22:36:13 +0100

    Prepare for authentication functionality

---
+++ E621Client/E621Credentials.cs
@@ -0,0 +1,18 @@
+﻿namespace Noppes.E621
+{
+    /// <summary>
+    /// The credentials used to gain access with to protected resources.
+    /// </summary>
+    internal class E621Credentials
+    {
+        public string Username { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public E621Credentials(string username, string apiKey)
+        {
+            Username = username;
+            ApiKey = apiKey;
+        }
+    }
+}
---
+++ E621Client/Extensions/ObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Noppes.E621.Extensions
+{
+    /// <summary>
+    /// Adds extra functionality to the <see cref="object"/> class.
+    /// </summary>
+    internal static class ObjectExtensions
+    {
+        /// <summary>
+        /// Gets all the public properties from the given object, retrieves their
+        /// name and value and puts them into a dictionary.
+        /// </summary>
+        public static IDictionary<string, object> ToDictionary(this object target)
+        {
+            return target.GetType()
+                .GetProperties()
+                .ToDictionary(p => p.Name, p => p.GetValue(target));
+        }
+    }
+}
