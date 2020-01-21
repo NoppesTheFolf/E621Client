31116a909cb71b02b5f66166351553a0e941951f
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-01-21 23:15:40 +0100

    Add authentication support

---
+++ E621Client/Area/Authentication/LoginResult.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Noppes.E621
+{
+    internal class LoginResult
+    {
+        public bool IsSuccess => Username != null && ApiKey != null;
+
+        [JsonProperty("name")]
+        public string? Username { get; set; }
+
+        [JsonProperty("password_hash")]
+        public string? ApiKey { get; set; }
+    }
+}
