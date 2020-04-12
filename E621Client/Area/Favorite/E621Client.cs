2b52d0edcb9197b86016c80d18714c70db8bc09c
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-04-12 21:36:09 +0200

    Update favorites area

--- E621Client/Area/Favorite/E621Client.cs
+++ E621Client/Area/Favorite/E621Client.cs
@@ -1,47 +1,57 @@
-﻿using Flurl.Http;
-using Newtonsoft.Json.Linq;
+﻿using Dawn;
+using Flurl.Http;
 using Noppes.E621.Extensions;
 using System;
-using System.Globalization;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 
 namespace Noppes.E621
 {
-    // Implements https://e621.net/help/show/api#favorites
     public partial class E621Client
     {
         /// <summary>
-        /// Adds a post to the user's favorites. Trying to add a post that does
-        /// not exist or is already a favorite, will silently fail.
+        /// The maximum possible number of favorites retrieved in a single call to <see cref="GetFavoritesAsync"/>.
         /// </summary>
-        /// <exception cref="E621ClientNotAuthenticatedException"></exception>
-        public Task CreateFavoriteAsync(Post post) =>
-            CreateFavoriteAsync(post.Id);
+        public static int FavoritesMaximum { get; } = 75;
+
+        private enum FavoriteAction
+        {
+            Add,
+            Remove
+        }
 
         /// <summary>
         /// Adds a post to the user's favorites. Trying to add a post that does
-        /// not exist or is already a favorite, will silently fail.
+        /// not exist or is already a favorite, will silently 'fail'.
         /// </summary>
         /// <exception cref="E621ClientNotAuthenticatedException"></exception>
-        public Task CreateFavoriteAsync(int postId) =>
-            FavoriteAsync(postId, FavoriteAction.Create);
+        public Task AddFavoriteAsync(Post post) =>
+            AddFavoriteAsync(post.Id);
+
+        /// <summary>
+        /// Adds a post to the user's favorites. Trying to add a post that does
+        /// not exist or is already a favorite, will silently 'fail'.
+        /// </summary>
+        /// <exception cref="E621ClientNotAuthenticatedException"></exception>
+        public Task AddFavoriteAsync(int postId) =>
+            FavoriteAsync(postId, FavoriteAction.Add);
 
         /// <summary>
         /// Removes a post from the user's favorites. Trying to remove a post that does
-        /// not exist or is not a favorite, will silently fail.
+        /// not exist or is not a favorite, will silently 'fail'.
         /// </summary>
         /// <exception cref="E621ClientNotAuthenticatedException"></exception>
-        public Task DestroyFavoriteAsync(Post post) =>
-            DestroyFavoriteAsync(post.Id);
+        public Task RemoveFavoriteAsync(Post post) =>
+            RemoveFavoriteAsync(post.Id);
 
         /// <summary>
         /// Removes a post from the user's favorites. Trying to remove a post that does
-        /// not exist or is not a favorite, will silently fail.
+        /// not exist or is not a favorite, will silently 'fail'.
         /// </summary>
         /// <exception cref="E621ClientNotAuthenticatedException"></exception>
-        public Task DestroyFavoriteAsync(int postId) =>
-            FavoriteAsync(postId, FavoriteAction.Destroy);
+        public Task RemoveFavoriteAsync(int postId) =>
+            FavoriteAsync(postId, FavoriteAction.Remove);
 
         private Task FavoriteAsync(int postId, FavoriteAction action)
         {
@@ -49,62 +59,52 @@ namespace Noppes.E621
             {
                 var requestUrl = action switch
                 {
-                    FavoriteAction.Create => "/favorite/create.json",
-                    FavoriteAction.Destroy => "/favorite/destroy.json",
+                    FavoriteAction.Add => "/favorites.json",
+                    FavoriteAction.Remove => $"/favorites/{postId}.json",
                     _ => throw new ArgumentOutOfRangeException(nameof(action))
                 };
 
-                // Locked: Post is already a favorite
-                // InternalServerError: Post does not exist
-                return FlurlClient.Request(requestUrl)
-                    .SetQueryParams(new
+                // UnprocessableEntity: Post is already a favorite
+                var request = FlurlClient.Request(requestUrl)
+                    .AllowHttpStatus(HttpStatusCode.UnprocessableEntity)
+                    .Authenticated(this);
+
+                return action switch
+                {
+                    FavoriteAction.Remove => request.DeleteAsync(),
+                    FavoriteAction.Add => request.PostJsonAsync(new
                     {
-                        id = postId.ToString(CultureInfo.InvariantCulture)
-                    })
-                    .AllowHttpStatus(HttpStatusCode.Locked, HttpStatusCode.InternalServerError)
-                    .PostAuthenticatedUrlEncodedAsync(this, new
-                    {
-                        id = postId.ToString(CultureInfo.InvariantCulture)
-                    });
+                        post_id = postId
+                    }),
+                    _ => throw new ArgumentOutOfRangeException(nameof(action))
+                };
             });
         }
 
-        private enum FavoriteAction
-        {
-            Create,
-            Destroy
-        }
-
         /// <summary>
-        /// Retrieve an array of usernames of users who favorited the post. Will return null
-        /// if the given post does not exist. The usernames can still be retrieved from
-        /// deleted posts.
+        /// Gets the currently logged-in user's favorited posts. The maximum possible number of
+        /// favorites retrieved in a single call, is defined at <see cref="FavoritesMaximum"/>.
         /// </summary>
-        public Task<string[]?> GetUsersWhoFavoritedPostAsync(Post post) =>
-            GetUsersWhoFavoritedPostAsync(post.Id);
-
-        /// <summary>
-        /// Retrieve an array of usernames of users who favorited the post. Will return null
-        /// if the given post does not exist. The usernames can still be retrieved from
-        /// deleted posts.
-        /// </summary>
-        public Task<string[]?> GetUsersWhoFavoritedPostAsync(int postId)
+        /// <param name="page">Pagination, page number.</param>
+        public Task<ICollection<Post>> GetFavoritesAsync(int? page = null)
         {
+            Guard.Argument(page, nameof(page)).Positive();
+
             return CatchAsync(() =>
             {
-                return FlurlClient.Request("/favorite/list_users.json")
-                    .SetQueryParams(new
-                    {
-                        id = postId
-                    })
-                    .GetJsonAsync(token =>
-                    {
-                        var usersCsv = token["favorited_users"].Value<string>();
+                var request = FlurlClient
+                    .Request("/favorites.json")
+                    .Authenticated(this);
 
-                        return string.IsNullOrWhiteSpace(usersCsv)
-                            ? Array.Empty<string>()
-                            : usersCsv.Split(',');
-                    }, HttpStatusCode.NotFound);
+                if (page != null)
+                {
+                    request = request.SetQueryParams(new
+                    {
+                        page
+                    });
+                }
+
+                return request.GetJsonAsync(token => token.SelectToken("posts").ToObject<ICollection<Post>>());
             });
         }
     }
