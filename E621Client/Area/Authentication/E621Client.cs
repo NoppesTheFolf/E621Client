e4f02b49fe06e6328d7e894776f50318247395d9
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-05-02 13:31:33 +0200

    Move authentication related properties to the appropriate class

--- E621Client/Area/Authentication/E621Client.cs
+++ E621Client/Area/Authentication/E621Client.cs
@@ -5,6 +5,16 @@ namespace Noppes.E621
     public partial class E621Client
     {
         /// <summary>
+        /// Whether or not a user is currently logged in.
+        /// </summary>
+        public bool HasLogin => Credentials != null;
+
+        /// <summary>
+        /// Credentials used to authenticate requests with.
+        /// </summary>
+        internal E621Credentials? Credentials { get; set; }
+
+        /// <summary>
         /// Logs the user in based on their username and API key. It will validate the provided credentials and return
         /// whether or not the log in was successful or not.
         /// </summary>
