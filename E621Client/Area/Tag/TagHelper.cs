1111df313065ff35621009dfdf17ceec8d57e03d
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-05-02 17:42:23 +0200

    Add missing TagHelper

---
+++ E621Client/Area/Tag/TagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Noppes.E621
+{
+    public static class TagHelper
+    {
+        public static string[] ParseSearchString(string? tags)
+        {
+            if (tags == null)
+                return Array.Empty<string>();
+
+            return tags
+                .Split(' ')
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .ToArray();
+        }
+    }
+}
