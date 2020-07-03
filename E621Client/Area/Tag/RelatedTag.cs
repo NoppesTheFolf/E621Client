682534939f1b474e7eb7feec550876e212b6c677
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-07-03 15:57:12 +0200

    Implement the retrieval of tags

---
+++ E621Client/Area/Tag/RelatedTag.cs
@@ -0,0 +1,18 @@
+﻿namespace Noppes.E621
+{
+    /// <summary>
+    /// Represents a tag that is related to some other tag.
+    /// </summary>
+    public class RelatedTag
+    {
+        /// <summary>
+        /// Name of the related tag.
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// "Weight" of the related tag. The larger the number, the stronger the connection.
+        /// </summary>
+        public int Weight { get; set; }
+    }
+}
---
+++ E621Client/Area/Tag/TagOrder.cs
@@ -0,0 +1,24 @@
+﻿namespace Noppes.E621
+{
+    /// <summary>
+    /// The orders in which tags can be sorted.
+    /// </summary>
+    public enum TagOrder
+    {
+        /// <summary>
+        /// Order by the date the tag was created.
+        /// </summary>
+        [Parameter("date")]
+        Date,
+        /// <summary>
+        /// Order by the number of posts that use the tag.
+        /// </summary>
+        [Parameter("count")]
+        Count,
+        /// <summary>
+        /// Order by the name of the tag.
+        /// </summary>
+        [Parameter("name")]
+        Name
+    }
+}
