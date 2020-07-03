e92cfc5900688a026836aa08c22e81ea4fd0cd15
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2020-07-03 15:57:12 +0200

    Implement the retrieval of tags

---
+++ E621Client/Converters/RelatedTagsConverter.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Noppes.E621.Converters
+{
+    /// <summary>
+    /// Converts between the annoying format related tags are represented in. 
+    /// </summary>
+    internal class RelatedTagsConverter : StringJsonConverter<ICollection<RelatedTag>>
+    {
+        private const string EmptyArray = "[]";
+
+        protected override ICollection<RelatedTag> ReadString(string value, Type objectType, ICollection<RelatedTag> existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (value == EmptyArray || string.IsNullOrWhiteSpace(value))
+                return new List<RelatedTag>();
+
+            var relatedTags = value.Split(' ');
+
+            if (relatedTags.Length % 2 == 1)
+                throw new ArgumentException("The provided string of related tags doesn't consist of an even number of pairs.", nameof(value));
+
+            return Enumerable.Range(0, relatedTags.Length)
+                .Where(i => i % 2 == 0)
+                .Select(i =>
+                {
+                    if (!int.TryParse(relatedTags[i + 1], out var weight))
+                        throw new ArgumentException("The provided string of related tags isn't in the expected format.", nameof(value));
+
+                    return new RelatedTag
+                    {
+                        Name = relatedTags[i],
+                        Weight = weight
+                    };
+                })
+                .ToList();
+        }
+
+        protected override string AsString(ICollection<RelatedTag> value)
+        {
+            return value.Count == 0 ? EmptyArray : string.Join(' ', value.Select(v => $"{v.Name} {v.Weight}"));
+        }
+    }
+}
