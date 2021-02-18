894486a85c29f06320d14878c237f37125bee94a
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-02-18 21:51:48 +0100

    Change SameAsSource to Has on post

--- E621Client/Area/Post/PostSampleImage.cs
+++ E621Client/Area/Post/PostSampleImage.cs
@@ -5,10 +5,10 @@ namespace Noppes.E621
     public class PostSampleImage : PostImage
     {
         /// <summary>
-        /// Whether the sample image is the exact same image as the source image.
+        /// Whether the sample image is not the same image as the source image.
         /// </summary>
         [JsonProperty("has", NullValueHandling = NullValueHandling.Ignore)]
-        public bool SameAsSource { get; set; } = true;
+        public bool Has { get; set; } = true;
 
         /* A null value is present in case e621 is unable to generate a sample.
          This is the case in Adobe Flash (SWF) files, for example. This means the given sample URL
