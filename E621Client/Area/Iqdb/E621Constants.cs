ca1a1ca00a9839126429f1386e0f8132df34cf35
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-03-21 15:39:07 +0100

    Spread constants across different multiple files

---
+++ E621Client/Area/Iqdb/E621Constants.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Noppes.E621
+{
+    // Contains constants related to e621's IQDB area.
+    public static partial class E621Constants
+    {
+        /// <summary>
+        /// The image formats IQDB supports. This <see cref="IReadOnlyDictionary{TKey,TValue}"/>
+        /// contains both the abbreviation of the image format (JPEG, PNG, GIF) and their
+        /// corresponding file extensions. However, you are unlikely to need this. Take a look at
+        /// <see cref="IqdbAllowedFormatNames"/> and <see cref="IqdbAllowedFormatExtensions"/> first.
+        /// </summary>
+        public static readonly IReadOnlyDictionary<string, string[]> IqdbAllowedFormats = new Dictionary<string, string[]>
+        {
+            {"JPEG", new[] {"jpg", "jpeg"}},
+            {"PNG", new[] {"png"}},
+            {"GIF", new[] {"gif"}}
+        };
+
+        /// <summary>
+        /// The names of the image formats IQDB supports.
+        /// </summary>
+        public static readonly IReadOnlyCollection<string> IqdbAllowedFormatNames = IqdbAllowedFormats
+            .Select(iaif => iaif.Key)
+            .ToHashSet();
+
+        /// <summary>
+        /// The file extensions of the image formats IQDB supports.
+        /// </summary>
+        public static readonly IReadOnlyCollection<string> IqdbAllowedFormatExtensions = IqdbAllowedFormats
+            .SelectMany(iaif => iaif.Value)
+            .Select(e => '.' + e)
+            .ToHashSet();
+    }
+}
---
+++ E621Client/Area/Tag/E621Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace Noppes.E621
+{
+    // Contains constants related to e621's tags area.
+    public static partial class E621Constants
+    {
+        /// <summary>
+        /// The maximum number of tags which can be retrieved in a single call to one of the
+        /// GetTagsAsync overloads.
+        /// </summary>
+        public static readonly int TagsMaximumLimit = 1000;
+
+        /// <summary>
+        /// The maximum allowed page number when making a call one of the GetTagsAsync overloads.
+        /// </summary>
+        public static readonly int TagsMaximumPage = 750;
+    }
+}
