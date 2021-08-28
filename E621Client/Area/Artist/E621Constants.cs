b79e7f60e76b6628afa954a56f12337c6af9c318
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-08-28 22:10:04 +0200

    Implement retrieval of artists

---
+++ E621Client/Area/Artist/E621Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace Noppes.E621
+{
+    // Contains constants related to e621's artists area.
+    public static partial class E621Constants
+    {
+        /// <summary>
+        /// The maximum number of artists which can be retrieved in a single call to <see cref="IE621Client.GetArtistsAsync"/>.
+        /// </summary>
+        public static readonly int ArtistsMaximumLimit = 320;
+
+        /// <summary>
+        /// The maximum allowed page number when making a call to <see cref="IE621Client.GetArtistsAsync"/>.
+        /// </summary>
+        public static readonly int ArtistsMaximumPage = 750;
+    }
+}
