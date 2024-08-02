f7a2c33d3df866e3167be51d2fabc722400684cd
GizmoCaritate <spam.gizmo@gmail.com>
2024-08-03 00:11:12 +0200

    Added the E6ai.net iamgeboard.

--- E621Client/Imageboard.cs
+++ E621Client/Imageboard.cs
@@ -14,7 +14,12 @@ namespace Noppes.E621
         /// <summary>
         /// Use e921 as source for API requests.
         /// </summary>
-        E926
+        E926,
+        
+        /// <summary>
+        /// Use e6ai as source for API requests.
+        /// </summary>
+        E6AI
     }
 
     internal static class ImageboardExtensions
@@ -25,6 +30,9 @@ namespace Noppes.E621
         private const string E921BaseUrlRegistrableDomain = "e926.net";
         private const string E921BaseUrl = "https://" + E921BaseUrlRegistrableDomain;
 
+        private const string E6AIBaseUrlRegistrableDomain = "e6ai.net";
+        private const string E6AIBaseUrl = "https://" + E6AIBaseUrlRegistrableDomain;
+
         /// <summary>
         /// Maps the imageboard to a base URL that can be for HTTP clients.
         /// </summary>
@@ -34,6 +42,7 @@ namespace Noppes.E621
             {
                 Imageboard.E621 => (E621BaseUrlRegistrableDomain, E621BaseUrl),
                 Imageboard.E926 => (E921BaseUrlRegistrableDomain, E921BaseUrl),
+                Imageboard.E6AI => (E6AIBaseUrlRegistrableDomain, E6AIBaseUrl),
                 _ => throw new ArgumentOutOfRangeException(nameof(imageboard))
             };
         }
