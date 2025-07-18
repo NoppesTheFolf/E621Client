2437310726a9c401daad967ab11de7fe2fb3254c
Jdbye <jdbye3@gmail.com>
2025-07-18 23:44:43 +0200

    Allow Name to be null in PoolNameJsonConverter

--- E621Client/Converters/PoolNameJsonConverter.cs
+++ E621Client/Converters/PoolNameJsonConverter.cs
@@ -10,10 +10,10 @@ namespace Noppes.E621.Converters
     /// </summary>
     internal class PoolNameJsonConverter : StringJsonConverter<string>
     {
-        protected override string ReadString(string value, Type objectType, string existingValue, bool hasExistingValue, JsonSerializer serializer) =>
+        protected override string ReadString(string value, Type objectType, string? existingValue, bool hasExistingValue, JsonSerializer serializer) =>
             value.Replace('_', ' ');
 
-        protected override string AsString(string value) =>
-            value.Replace(' ', '_');
+        protected override string AsString(string? value) =>
+            value?.Replace(' ', '_') ?? String.Empty;
     }
 }
