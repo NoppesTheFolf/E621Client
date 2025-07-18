0c2618033cdbf776e6a8c55aff36a0227b73010c
Jdbye <jdbye3@gmail.com>
2025-07-19 00:03:21 +0200

    Add Flag API endpoint

---
+++ E621Client/Area/Flag/IE621Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Noppes.E621
+{
+    // Interface for the Flag part of the E621Client (added by Jdbye)
+    public partial interface IE621Client
+    {
+        /// <summary>
+        /// Retrieves flags for one or more posts, with their descriptions
+        /// </summary>
+        /// <param name="postId">The ID of a post to check.</param>
+        public Task<IList<Flag>?> GetFlagsAsync(int postId);
+
+        /// <summary>
+        /// Retrieves flags for one or more posts, with their descriptions
+        /// </summary>
+        /// <param name="postId">The IDs of one or more posts to check.</param>
+        public Task<IList<Flag>?> GetFlagsAsync(IEnumerable<int> postIds, int limit = 320);
+    }
+}
