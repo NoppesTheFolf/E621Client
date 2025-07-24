69c9b2a0f92528db310e66f4f24b603300a94836
Jdbye <jdbye3@gmail.com>
2025-07-24 20:11:10 +0200

    Include IqdbPost even when post is deleted as the metadata can still be useful

--- E621Client/Area/Iqdb/IqdbPost.cs
+++ E621Client/Area/Iqdb/IqdbPost.cs
@@ -194,8 +194,7 @@ namespace Noppes.E621
                 Duration = Duration != null ? TimeSpan.FromSeconds((float)Duration) : (TimeSpan?)null
         };
 
-            if (IsDeleted)
-                return post;
+            // Don't ignore result on deleted posts, we still want the metadata
 
 #pragma warning disable CS8601 // Possible null reference assignment. The values will only be null if the post has been deleted.
             post.File = new PostFileImage
