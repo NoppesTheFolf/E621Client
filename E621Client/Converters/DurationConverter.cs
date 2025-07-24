fb61134727b7bcb144adeadeefe1a554ca8da047
Jdbye <jdbye3@gmail.com>
2025-07-24 21:27:20 +0200

    Code formatting changes in DurationConverter

--- E621Client/Converters/DurationConverter.cs
+++ E621Client/Converters/DurationConverter.cs
@@ -10,15 +10,16 @@ namespace Noppes.E621.Converters
     {
         public override void WriteJson(JsonWriter writer, TimeSpan? value, JsonSerializer serializer)
         {
-            if (value is null) writer.WriteNull();
-            // Write TimeSpan as total seconds (as a double)
-            else writer.WriteValue(value.Value.TotalSeconds);
+        if (value is null)
+            writer.WriteNull();
+        else // Write TimeSpan as total seconds (as a double)
+            writer.WriteValue(value.Value.TotalSeconds);
         }
 
         public override TimeSpan? ReadJson(JsonReader reader, Type objectType, TimeSpan? existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            if (reader.TokenType == JsonToken.Null) return null;
-
+            if (reader.TokenType == JsonToken.Null)
+                return null;
             if (reader.TokenType == JsonToken.Float || reader.TokenType == JsonToken.Integer)
                 return TimeSpan.FromSeconds(Convert.ToDouble(reader.Value));
 
