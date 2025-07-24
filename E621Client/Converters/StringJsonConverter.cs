7e277a19f6169d017dddee5811ec564917134348
Jdbye <jdbye3@gmail.com>
2025-07-24 19:03:22 +0200

    Undo some changes that C# 8.0 complains about

--- E621Client/Converters/StringJsonConverter.cs
+++ E621Client/Converters/StringJsonConverter.cs
@@ -10,25 +10,25 @@ namespace Noppes.E621.Converters
     /// <typeparam name="T">Type to convert to and from.</typeparam>
     internal abstract class StringJsonConverter<T> : JsonConverter<T>
     {
-        public sealed override T ReadJson(JsonReader reader, Type objectType, T? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        public sealed override T ReadJson(JsonReader reader, Type objectType, T existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
             if (reader.Value == null)
                 return ReadString(String.Empty, objectType, existingValue, hasExistingValue, serializer);
-            if (reader.Value is not string value)
+            if (!(reader.Value is string value))
                 throw new InvalidOperationException($"Value from {reader.GetType().Name} must be a string.");
 
             return ReadString(value, objectType, existingValue, hasExistingValue, serializer);
         }
 
-        protected abstract T ReadString(string value, Type objectType, T? existingValue, bool hasExistingValue, JsonSerializer serializer);
+        protected abstract T ReadString(string value, Type objectType, T existingValue, bool hasExistingValue, JsonSerializer serializer);
 
-        public sealed override void WriteJson(JsonWriter writer, T? value, JsonSerializer serializer)
+        public sealed override void WriteJson(JsonWriter writer, T value, JsonSerializer serializer)
         {
             var valueAsString = AsString(value);
 
             serializer.Serialize(writer, valueAsString);
         }
 
-        protected abstract string AsString(T? value);
+        protected abstract string AsString(T value);
     }
 }
