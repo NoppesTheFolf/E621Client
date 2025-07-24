edef2c19675178092f6ffe29d16271cd2e37da36
Jdbye <jdbye3@gmail.com>
2025-07-24 22:29:21 +0200

    Use ICollection instead of IList

--- E621Client/Area/Flag/E621Client.cs
+++ E621Client/Area/Flag/E621Client.cs
@@ -14,8 +14,8 @@ namespace Noppes.E621
 {
     public partial class E621Client
     {
-        public Task<IList<Flag>?> GetFlagsAsync(int postId) => GetFlagsAsync(new int[] { postId });
-        public Task<IList<Flag>?> GetFlagsAsync(IEnumerable<int> postIds, int limit = 320)
+        public Task<ICollection<Flag>?> GetFlagsAsync(int postId) => GetFlagsAsync(new int[] { postId });
+        public Task<ICollection<Flag>?> GetFlagsAsync(IEnumerable<int> postIds, int limit = 320)
         {
             Guard.Argument(limit, nameof(limit)).InRange(1, E621Constants.PostsMaximumLimit);
 
@@ -28,7 +28,7 @@ namespace Noppes.E621
                     .AuthenticatedIfPossible(this)
                     .SetQueryParam("limit", limit)
                     .SetQueryParam("search[post_id]", string.Join(',', postIds))
-                    .GetJsonAsync(token => token.ToObject<IList<Flag>>()));
+                    .GetJsonAsync(token => token.ToObject<ICollection<Flag>>()));
 #pragma warning restore CS8621 // Nullability of reference types in return type doesn't match the target delegate (possibly because of nullability attributes).
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
 #pragma warning restore CS8634 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'class' constraint.
