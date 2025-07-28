eb7e263a0b788debf4aa5b91cc5a7ca092b64c88
Jdbye <jdbye3@gmail.com>
2025-07-28 08:40:11 +0200

    Revert changes to GetPathExtensionWithoutDot

--- E621Client/Extensions/StringExtensions.cs
+++ E621Client/Extensions/StringExtensions.cs
@@ -8,16 +8,12 @@ namespace Noppes.E621.Extensions
     /// </summary>
     internal static class StringExtensions
     {
-        public static string GetPathExtensionWithoutDot(this string value, string? defaultValue = null)
+        public static string GetPathExtensionWithoutDot(this string value)
         {
             string extensionWithDot = Path.GetExtension(value).ToLower();
 
             if (string.IsNullOrEmpty(extensionWithDot))
-            {
-                if (defaultValue != null)
-                    return defaultValue.TrimStart('.');
                 throw new ArgumentException($"{nameof(value)} is not a path with an extension.", nameof(value));
-            }
 
             return extensionWithDot.Substring(1, extensionWithDot.Length - 1);
         }
